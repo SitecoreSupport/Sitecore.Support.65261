5f255d683c90ccb1de41113f43a56844c72ae351
DK\mnas <mnas@sitecore.net>
2017-02-13 14:17:40 +0800

    Commit patch

---
+++ src/Sitecore.Support.65261/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.65261")]
+[assembly: AssemblyProduct("Sitecore.Support.65261")]
+[assembly: ComVisible(false)]
